ec4ebec048b167e9984e031b738887cb957723c2
Wifsimster <wifsimster@gmail.com>
2016-02-11 23:01:09 +0100

    Change algo
    Now the device wait for request from Jeedom

--- sendData.lua
+++ sendData.lua
@@ -1,4 +1,3 @@
-
 require('config')
 
 alti=14 -- Set your altitude in meters
@@ -8,43 +7,38 @@ temp=0  -- Temperature
 pres=0 	-- Pressure
 oss=0   -- Over sampling setting
 
-function ReadBMP()
+function readBMP()
+    result = {}
 	bmp180 = require("bmp180")
 	bmp180.init(sda, scl)
 	bmp180.read(oss)
-	temp = bmp180.getTemperature()/10
-	pres = bmp180.getPressure()/100+alti/8.43
-	print("Pressure:    "..string.format("%.1f",pres).." hPa")
-	print("Temperature: "..string.format("%.1f",temp).." C")
-	print(" ")
-
+	result["temp"] = bmp180.getTemperature()/10
+	result["pres"] = bmp180.getPressure()/100+alti/8.43   
     -- Release bmp180 module
 	bmp180 = nil
 	package.loaded["bmp180"]=nil
-
-    -- HTTP request to Jeedom - Send temperature value
-    conn=net.createConnection(net.TCP, 0)
-    conn:on("receive", function(conn, payload) print(payload) end )
-    conn:connect(80,HOST)
-    conn:send("GET /core/api/jeeApi.php?apikey="..API.."&type=virtual&id=535&value="..string.format("%.1f",temp).."HTTP/1.1\r\n")
-    conn:send("Host: localhost\r\n") 
-    conn:send("Accept: */*\r\n") 
-    conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
-    conn:send("\r\n")
-    
-    -- HTTP request to Jeedom - Send pression value
-    conn=net.createConnection(net.TCP, 0)
-    conn:on("receive", function(conn, payload) print(payload) end )
-    conn:connect(80,HOST)
-    conn:send("GET /core/api/jeeApi.php?apikey="..API.."&type=virtual&id=536&value="..string.format("%.1f",pres).."HTTP/1.1\r\n")
-    conn:send("Host: localhost\r\n") 
-    conn:send("Accept: */*\r\n") 
-    conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
-    conn:send("\r\n")
+    return result
 end
 
--- First reading data
-ReadBMP()
-
--- Periodic reading of the sensor
-tmr.alarm(1,15000,1, function()ReadBMP()end)
+-- HTTP server 
+srv=net.createServer(net.TCP)
+srv:listen(80,function(conn)
+    conn:on("receive", function(client,request)    
+        local response = "HTTP/1.1 200 OK\r\n\r\nOK"
+        if(string.find(request, "/temp") ~= nil) then
+            response = readBMP().temp
+        end
+        if(string.find(request, "/pres") ~= nil) then
+            response = readBMP().pres
+        end
+        if(string.find(request, "/reset") ~= nil) then
+            node.restart()
+        end
+        -- Send response
+        print(response)
+        conn:send(response, function()
+            conn:close()
+        end)
+        collectgarbage();
+    end)
+end)
